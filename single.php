b4e48b869258e43ed845e6333fb30ca6f3dffe44
Isidora Kalicanin <isidorakalicanin@madeonline.agency>
2021-09-30 16:11:05 +0200

    recent and related posts added

--- single.php
+++ single.php
@@ -176,6 +176,75 @@ $container = get_theme_mod( 'understrap_container_type' );
                                             </div>
                                             <!-- // section  -->
 
+                                            <?php elseif( get_row_layout() == 'table' ): ?>
+
+                                                <table style="width:100%" class="single-table">
+                                                    <thead>
+                                                        <tr role="row">
+
+                                                        <?php
+
+                                                            // check if the repeater field has rows of data
+                                                            if(have_rows('table_head_cells')):
+
+                                                                // loop through the rows of data
+                                                                while(have_rows('table_head_cells')) : the_row();
+
+                                                                    $hlabel = get_sub_field('table_cell_label_thead');
+
+                                                                    ?>  
+
+                                                                    <th tabindex="0" rowspan="1" colspan="1"><?php echo $hlabel; ?></th>
+
+                                                                <?php endwhile;
+
+                                                            else :
+                                                                echo 'No data';
+                                                            endif;
+                                                            ?>
+
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+
+                                                    <?php while ( have_posts() ) : the_post(); ?>
+
+                                                        <?php 
+
+                                                        // check for rows (parent repeater)
+                                                        if( have_rows('table_body_row') ): ?>
+                                                            
+                                                            <?php 
+
+                                                            // loop through rows (parent repeater)
+                                                            while( have_rows('table_body_row') ): the_row(); ?>
+
+                                                                    <?php 
+
+                                                                    // check for rows (sub repeater)
+                                                                    if( have_rows('table_body_cells') ): ?>
+                                                                        <tr>
+                                                                            <?php 
+
+                                                                            // loop through rows (sub repeater)
+                                                                            while( have_rows('table_body_cells') ): the_row();
+
+                                                                                
+                                                                                ?>
+                                                                                <td><?php the_sub_field('table_cell_label_tbody'); ?></td>
+                                                                            <?php endwhile; ?>
+                                                                        </tr>
+                                                                    <?php endif; //if( get_sub_field('') ): ?>
+
+                                                            <?php endwhile; // while( has_sub_field('') ): ?>
+                                                                
+                                                        <?php endif; // if( get_field('') ): ?>
+
+                                                        <?php endwhile; // end of the loop. ?>
+                                                        
+                                                    </tbody>
+                                                </table>
+
                                     <?php endif;
 
                                 endwhile;
@@ -230,7 +299,69 @@ $container = get_theme_mod( 'understrap_container_type' );
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2>Similar posts</h2>
+                    <h2>Recent posts</h2>
+                    <div class="posts-list">
+                        <div class="row">
+
+                        <?php
+                            $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 3) ); ?>  
+                            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+                                        <div class="col-md-4">
+                                            <div class="posts-item">
+                                                <div class="blog-photo">
+                                                <a href="<?php the_permalink(); ?>">
+                                                <?php 
+                                                    $values = get_field( 'featured_image_blog' );
+                                                    if ( $values ) { ?>
+
+                                                        <?php
+                                                        $imageID = get_field('featured_image_blog');
+                                                        $image = wp_get_attachment_image_src( $imageID, 'blog-image' );
+                                                        $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                                        ?> 
+
+                                                        <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" /> 
+
+                                                    <?php 
+                                                    } else { ?>
+
+                                                        <img src="<?php bloginfo('template_directory'); ?>/img/misc/placeholder.jpg" alt="">
+
+                                                    <?php } ?></a>
+                                                </div>
+                                                <!-- /.blog-photo -->
+                                                <div class="blog-title">
+                                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                                </div>
+                                                <!-- /.blog-title -->
+                                            </div>
+                                            <!-- /.posts-item -->
+                                        </div>
+                                        <!-- /.col-md-4 -->
+
+                                <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?>    
+                        <?php wp_reset_query(); ?>
+
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.posts-list -->
+                </div>
+                <!-- /.col-md-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /.similar-posts -->
+
+    <section class="similar-posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Related posts</h2>
                     <div class="posts-list">
                         <div class="row">
 
@@ -252,6 +383,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                                         <div class="col-md-4">
                                             <div class="posts-item">
                                                 <div class="blog-photo">
+                                                <a href="<?php the_permalink(); ?>">
                                                 <?php 
                                                     $values = get_field( 'featured_image_blog' );
                                                     if ( $values ) { ?>
@@ -269,7 +401,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                                                         <img src="<?php bloginfo('template_directory'); ?>/img/misc/placeholder.jpg" alt="">
 
-                                                    <?php } ?>
+                                                    <?php } ?></a>
                                                 </div>
                                                 <!-- /.blog-photo -->
                                                 <div class="blog-title">
@@ -304,4 +436,4 @@ $container = get_theme_mod( 'understrap_container_type' );
     <!-- /.similar-posts -->
    
 <?php
-get_footer();
+get_footer(); ?>
